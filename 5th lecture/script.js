dc245c74c4d340004749d0291b2a0d6f89ad192a
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2024-11-19 10:53:36 +0500

    Arrow Function

--- 5th lecture/script.js
+++ 5th lecture/script.js
@@ -8,3 +8,21 @@ let displayResult = (a,b) => {
 }
 let result = displayResult(6,10);
 console.log(result);
+
+
+let calculation = (a,b) =>{
+    let c = a + b;
+    let d = a - b;
+    let e = a * b;
+    let f = a / b;
+    let g = a % b ;
+    return[c,d,e,f,g];
+}
+
+let ans = calculation(25,10);
+console.log(ans);
+console.log("sum of a and b is :" + ans[0]);
+console.log("Difference of a and b is :" + ans[1]);
+console.log("Product of a and b is :" + ans[2]);
+console.log("Division of a and b is :" + ans[3]);
+console.log("Reminder of a and b is :" + ans[4]);
