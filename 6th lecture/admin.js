5c1487aacc85499d41d611541062a14f9377138e
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2024-11-26 12:06:47 +0500

    Regular Expressions

--- 6th lecture/admin.js
+++ 6th lecture/admin.js
@@ -26,3 +26,47 @@ $(document).ready(function () {
         window.location.href = "login.html";  
     });
 });
+
+$(document).ready(function () {
+    // Check if user data exists in localStorage
+    let storedUserData = JSON.parse(localStorage.getItem("userData"));
+
+    if (!storedUserData) {
+    
+        alert("You need to log in first.");
+        window.location.href = "login.html"; 
+    } else {
+    
+        $("#userDetails").html(`
+            <h4>Name: ${storedUserData.name}</h4>
+            <h4>Email: ${storedUserData.email}</h4>
+            <h4>Password: ${storedUserData.password}</h4>
+        `);
+    }
+});
+
+$(document).ready(function () {
+    // Check if user is logged in
+    let isLoggedIn = localStorage.getItem("isLoggedIn");
+    let storedUserData = JSON.parse(localStorage.getItem("userData"));
+
+    if (!isLoggedIn || isLoggedIn === "false" || !storedUserData) {
+        alert("You need to log in first.");
+        window.location.href = "login.html";
+    } else {
+        
+        $("#userDetails").html(`
+            <h4>Name: ${storedUserData.name}</h4>
+            <h4>Email: ${storedUserData.email}</h4>
+        `);
+    }
+
+    // Logout Button
+    $("#logoutBtn").click(function () {
+        localStorage.setItem("isLoggedIn", "false"); 
+        alert("You have logged out successfully.");
+        window.location.href = "login.html"; 
+    });
+});
+
+
